aed7de0f7c5ad72f5604dc1a834928770ee8b8a1
Jean-Yves GARCIN <114491721+jeanyvesgarcin@users.noreply.github.com>
2023-06-10 10:10:06 +0200

    Add files via upload

---
+++ Day40 - Project7/Day40 - Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Day40 - Project7
+//
+//  Created by Jean-Yves Garcin on 10/06/2023.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+    
+}
---
+++ Day40 - Project7/Day40 - Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Day40 - Project7
+//
+//  Created by Jean-Yves Garcin on 10/06/2023.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+    
+}
