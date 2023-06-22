d29186b6845ffce71fedcdd18ae428dd51f72420
Jean-Yves GARCIN <114491721+jeanyvesgarcin@users.noreply.github.com>
2023-06-22 10:27:22 +0200

    Add files via upload

---
+++ Day60 - Project16/Project16/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  Day60- Project16
+//
+//  Created by Jean-Yves Garcin on 22/06/2023.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
